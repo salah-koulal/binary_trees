68ece941f4dd43e62022c5acc483b94c6c8d3248
salah-koulal <salah.rajawi289@gmail.com>
2023-11-01 13:39:13 +0100

    Inserting Left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,8 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
-
+	if (!parent)
+		return (NULL);
 	newNode = malloc(sizeof(binary_tree_t));
 	if (!newNode)
 		return (NULL);
