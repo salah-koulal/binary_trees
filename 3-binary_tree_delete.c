b4f71dbe6e417cca351947b90c53b0b0452cb034
salah-koulal <salah.rajawi289@gmail.com>
2023-11-02 00:51:39 +0100

    fixing betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete : Delete an entire binary tree
- * @tree : a pointer the root of the tree to delete
+ *binary_tree_delete - deletes an entire binary tree
+ *@tree: a pointer to the root node of the tree to delete
  */
 
 void binary_tree_delete(binary_tree_t *tree)
