3f98429e77bb236f91b9cb29fc5ae18145418c13
mouadnait <moadnait6@gmail.com>
2023-11-01 16:08:51 +0100

    12. Leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts leaf nodes
+ * @tree: tree to measure
+ * Return: number of leaf nodes
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	size = binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left);
+	return (size);
+}
